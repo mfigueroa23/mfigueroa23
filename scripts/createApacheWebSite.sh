8ba246a533f270e52ab1613c5e0f59722bb0538b
 Marco Figueroa <marcofigueroa23@icloud.com>
2024-06-10 21:48:43 -0400

    fix: recomended restart & more time to sleep

--- scripts/createApacheWebSite.sh
+++ scripts/createApacheWebSite.sh
@@ -4,7 +4,7 @@
 echo '\n[info]: Instalando paquetes...\n'
 sudo apt update -y && sudo apt-get install w3m apache2 -y
 
-sleep 1
+sleep 2
 
 # realizando creacion del vHost
 echo '\n[info]: Creando el vHost...\n'
@@ -14,7 +14,13 @@ sed -i '10 a         ServerName www.exmaple.local' www1_example_local.conf
 sed -i 's/html/example/g' www1_example_local.conf
 sudo a2ensite www1_example_local.conf
 
-sleep 1
+sleep 2
+
+# Reiniciando servicio
+echo '\n[info]: Reiniciando servicio...\n'
+systemctl restart apache2
+
+sleep 2
 
 # creamos los archivos para www-data
 echo '\n[info]: Creando el directorio para www-data...\n'
@@ -24,22 +30,16 @@ touch ./example/index.html
 echo '<h1>Hola!!!, el sitio <a href="http://www.example.local">http://www.example.local<a/> esta operativo.<h1/>' > ./example/index.html
 sudo chown -R www-data: example
 
-sleep 1
+sleep 2
 
 # Agregando el DNS interno
 echo '\n[info]: Agregando al DNS interno el sitio...\n'
 cd /etc
 sed -i '3 a 127.0.0.1 www.example.local' hosts
 
-sleep 1
-
-# Reiniciando servicio
-echo '\n[info]: Reiniciando servicio...\n'
-service apache2 restart
-
-sleep 1
+sleep 2
 
 # Validando la web con w3m
 echo '\n[info]: Verificando sitio con w3m, pulsar Q para salir.\n'
-sleep 1
+sleep 2
 w3m http://www.example.local
