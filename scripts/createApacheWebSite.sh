ea4975720b21da3a63773f7a372602e8809e3993
 Marco Figueroa <marcofigueroa23@icloud.com>
2024-06-10 21:37:14 -0400

    fix: typo errors

--- scripts/createApacheWebSite.sh
+++ scripts/createApacheWebSite.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-# Garantizamos el super usuario
-sudo su
-
 # Intalando paquetes necesarios
 echo '\n[info]: Instalando paquetes...\n'
-sudo apt update -y && sudo apt-get intall w3m apache2 -y
+sudo apt update -y && sudo apt-get install w3m apache2 -y
 
 sleep 1
 
