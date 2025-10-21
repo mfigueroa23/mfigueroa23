77b3daee7af566e7ad2e9c4761dc89ee2ead0bf5
 Marco Figueroa <marcofigueroa23@icloud.com>
2025-10-21 01:45:24 -0300

    refactor: docker installation ans post install

--- scripts/DockerInstall.sh
+++ scripts/DockerInstall.sh
@@ -1,9 +1,10 @@
 # Removing unofficial packages
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt remove $pkg; done
 
 # Add Docker's official GPG key:
-sudo apt-get update
-sudo apt-get install ca-certificates curl
+sudo apt-get update -y
+sudo apt-get install ca-certificates curl -y
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -13,10 +14,19 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
+sudo apt-get update -y
 
 # Installing the latest version
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y 
 
-# Verify status
+# Creating docker group
+sudo groupadd docker
+
+# Adding user to docker group
+sudo usermod -aG docker $USER
+
+# Activating changes to groups
+newgrp docker
+
+# Verifying installation
 sudo systemctl status docker
